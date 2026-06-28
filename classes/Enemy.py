ebe979689a16c508fd16715f2e40578f740b8bf9
Jason <hnhjknmn@gmail.com>
2026-06-28 13:13:27 -0400

    basic enemy class, mostly copied from player class

---
+++ classes/Enemy.py
@@ -0,0 +1,35 @@
+import json
+from pathlib import Path
+
+BASE_DIR = Path(__file__).parent.parent
+REALM_DATA = BASE_DIR / "data" / "realmData.json"
+
+with open(REALM_DATA, "r") as file:
+    realms = json.load(file)
+
+class Enemy:
+    def __init__(
+        self, 
+        name, 
+        realm,
+        minor_realm
+        ):
+        self.name = name
+        self.realm = realm
+        self.minor_realm = minor_realm
+        self.health = 1
+        self.attack = 1
+        self.defense = 1
+
+    def get_realm_data(self):
+        current_realm_index = realms_dict[self.realm]
+        return realms["realms"][current_realm_index]
+
+
+    def update_stats(self):
+        realm_data = self.get_realm_data()
+        multiplier = realm_data["stage_multiplier"] ** (self.minor_realm - 1)
+
+        self.health = int(realm_data["base_hp"] * multiplier)
+        self.attack = int(realm_data["base_attack"] * multiplier)
+        self.defense = int(realm_data["base_defense"] * multiplier)
